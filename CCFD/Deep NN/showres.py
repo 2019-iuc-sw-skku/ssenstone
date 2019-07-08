5aa46426ff60a9d98ae45f11a273605a29518f1d
Seniorious <wldnjs1323@naver.com>
2019-07-08 15:59:52 +0900

    add deep NN

---
+++ CCFD/Deep NN/showres.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from keras.models import load_model
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+LABELS = ["Normal","Fraud"]
+RANDOM_SEED = 1398
+TEST_PCT = 0.8
+
+df = pd.read_csv("./creditcard.csv")
+df_norm = df
+df_norm['Time'] = StandardScaler().fit_transform(df_norm['Time'].values.reshape(-1, 1))
+df_norm['Amount'] = StandardScaler().fit_transform(df_norm['Amount'].values.reshape(-1, 1))
+
+train_x, test_x = train_test_split(df_norm, test_size=TEST_PCT, random_state=RANDOM_SEED)
+train_x = train_x[train_x.Class == 0]
+train_x = train_x.drop(['Class'], axis=1)
+
+test_y = test_x['Class']
+test_x = test_x.drop(['Class'], axis=1)
+
+train_x = train_x.values
+test_x = test_x.values
+
+autoencoder = load_model('fraud_dl.cp')
+threshold_fixed = 5
+
+test_x_predictions = autoencoder.predict(test_x)
+mse = np.mean(np.power(test_x - test_x_predictions, 2), axis=1)
+error_df = pd.DataFrame({'Reconstruction_error': mse, 'True_class': test_y})
+
+pred_y = [1 if e > threshold_fixed else 0 for e in error_df.Reconstruction_error.values]
+conf_matrix = confusion_matrix(error_df.True_class, pred_y)
+
+plt.figure(figsize=(12, 12))
+sns.heatmap(conf_matrix, xticklabels=LABELS, yticklabels=LABELS, annot=True, fmt="d");
+plt.title("Confusion matrix")
+plt.ylabel('True class')
+plt.xlabel('Predicted class')
+plt.show()
