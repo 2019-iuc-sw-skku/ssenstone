358e3984447f4df6a7da105adcbd52c58d98aed2
Mysigyeong <woobs79@naver.com>
2019-07-17 14:50:29 +0900

    trainer_GUI 완성

--- CCFD/trainer_GUI.py
+++ CCFD/trainer_GUI.py
@@ -1,6 +1,19 @@
+'''
+original data: 원래 가지고 있던 데이터
+new data: 새로 들어온 데이터
+
+original data의 끝에 new data들을 붙여서 original data를 업데이트 한 후
+trainer에게 학습하는데 필요한 인자들과 같이 전달을 합니다.
+'''
+
 import sys
+import csv
 import trainer
+from model_names import ModelNames
 from PyQt5.QtWidgets import *
+
+class FilePathError(Exception):
+    pass
 
 class TrainerGUI(QWidget):
     def __init__(self):
@@ -10,19 +23,42 @@ class TrainerGUI(QWidget):
 
     def setupUI(self):
         self.setGeometry(800, 200, 600, 600)
-        self.setWindowTitle("Classifier")
+        self.setWindowTitle("Trainer")
 
         grid = QGridLayout()
 
-        grid.addWidget(self.browseData(), 0, 0)
-        grid.addWidget(self.modelSetting(), 1, 0)
-        grid.addWidget(self.trainStart(), 2, 0)
+        grid.addWidget(self.browseOriginalData(), 0, 0)
+        grid.addWidget(self.browseNewData(), 1, 0)
+        grid.addWidget(self.modelSetting(), 2, 0)
+        grid.addWidget(self.trainStart(), 3, 0)
 
         self.setLayout(grid)
         self.show()
 
-    def browseData(self):
-        groupbox = QGroupBox('Browse Data Sets')
+    def browseOriginalData(self):
+        groupbox = QGroupBox('Browse Original Data Set')
+
+        grid = QGridLayout()
+
+        self.le = QLineEdit()
+        self.le.setReadOnly(True)
+
+        btn = QPushButton("Browse..", self)
+        btn.clicked.connect(self.originalButton)
+
+        grid.addWidget(self.le, 0, 0)
+        grid.addWidget(btn, 0, 1)
+
+        groupbox.setLayout(grid)
+
+        return groupbox
+
+    def originalButton(self):
+        fname = QFileDialog.getOpenFileName(self, "Load File", ".", "csv(*.csv)")
+        self.le.setText(fname[0])
+
+    def browseNewData(self):
+        groupbox = QGroupBox('Browse New Data Sets')
 
         grid = QGridLayout()
 
@@ -41,7 +77,7 @@ class TrainerGUI(QWidget):
         return groupbox
 
     def buttonClicked(self):
-        fname = QFileDialog.getOpenFileNames(self, "Load File", ".", "csv(*.csv)")
+        fname = QFileDialog.getOpenFileNames(self, "Load Files", ".", "csv(*.csv)")
         for i in fname[0]:
             self.te.append(i)
 
@@ -49,14 +85,20 @@ class TrainerGUI(QWidget):
         groupbox = QGroupBox('Select Model')
 
         self.modelgrid = QGridLayout()
-
+        self.selectgrid = QGridLayout()
         self.makeOptionGrid()
 
         self.cb = QComboBox(self)
         self.cb.addItems(['Random Forest', 'Autoencoded Deep Learning', 'Support Vector Machine', 'Logistic Regression'])
         self.cb.currentIndexChanged.connect(self.modelChanged)
 
-        self.modelgrid.addWidget(self.cb, 0, 0)
+        self.pcttext = QLineEdit('0.4')
+
+        self.selectgrid.addWidget(self.cb, 0, 0)
+        self.selectgrid.addWidget(QLabel('train_pct ( 0 < x < 1 )', self), 0, 1)
+        self.selectgrid.addWidget(self.pcttext, 0, 2)
+
+        self.modelgrid.addLayout(self.selectgrid, 0, 0)
         self.modelgrid.addLayout(self.optiongrid, 1, 0)
 
         groupbox.setLayout(self.modelgrid)
@@ -65,81 +107,72 @@ class TrainerGUI(QWidget):
 
     def modelChanged(self):
         if self.cb.currentText() == 'Random Forest':
-            self.rfoption.show()
-            self.rftext.show()
-
-            self.dloption.hide()
-            self.dltext.hide()
-
-            self.svmoption.hide()
-            self.svmcombo.hide()
-            self.svmoption1.hide()
-            self.svmtext1.hide()
-
-            self.lroption.hide()
-            self.lrtext.hide()
-            self.lroption1.hide()
-            self.lrtext1.hide()
-            self.lroption2.hide()
-            self.lrcombo.hide()
+            self.rfshow()
+            self.dlhide()
+            self.svmhide()
+            self.lrhide()
 
         elif self.cb.currentText() == 'Autoencoded Deep Learning':
-            self.rfoption.hide()
-            self.rftext.hide()
-
-            self.dloption.show()
-            self.dltext.show()
-
-            self.svmoption.hide()
-            self.svmcombo.hide()
-            self.svmoption1.hide()
-            self.svmtext1.hide()
-
-            self.lroption.hide()
-            self.lrtext.hide()
-            self.lroption1.hide()
-            self.lrtext1.hide()
-            self.lroption2.hide()
-            self.lrcombo.hide()
+            self.rfhide()
+            self.dlshow()
+            self.svmhide()
+            self.lrhide()
 
         elif self.cb.currentText() == 'Support Vector Machine':
-            self.rfoption.hide()
-            self.rftext.hide()
-
-            self.dloption.hide()
-            self.dltext.hide()
-
-            self.svmoption.show()
-            self.svmcombo.show()
-            self.svmoption1.show()
-            self.svmtext1.show()
-
-            self.lroption.hide()
-            self.lrtext.hide()
-            self.lroption1.hide()
-            self.lrtext1.hide()
-            self.lroption2.hide()
-            self.lrcombo.hide()
+            self.rfhide()
+            self.dlhide()
+            self.svmshow()
+            self.lrhide()
 
         elif self.cb.currentText() == 'Logistic Regression':
-            self.rfoption.hide()
-            self.rftext.hide()
+            self.rfhide()
+            self.dlhide()
+            self.svmhide()
+            self.lrshow()
 
-            self.dloption.hide()
-            self.dltext.hide()
+    def rfshow(self):
+        self.rfoption.show()
+        self.rftext.show()
 
-            self.svmoption.hide()
-            self.svmcombo.hide()
-            self.svmoption1.hide()
-            self.svmtext1.hide()
+    def rfhide(self):
+        self.rfoption.hide()
+        self.rftext.hide()
 
-            self.lroption.show()
-            self.lrtext.show()
-            self.lroption1.show()
-            self.lrtext1.show()
-            self.lroption2.show()
-            self.lrcombo.show()
-            
+    def dlshow(self):
+        self.dloption.show()
+        self.dltext.show()
+
+    def dlhide(self):
+        self.dloption.hide()
+        self.dltext.hide()
+
+    def svmshow(self):
+        self.svmoption.show()
+        self.svmcombo.show()
+        self.svmoption1.show()
+        self.svmtext1.show()
+
+    def svmhide(self):
+        self.svmoption.hide()
+        self.svmcombo.hide()
+        self.svmoption1.hide()
+        self.svmtext1.hide()
+
+    def lrshow(self):
+        self.lroption.show()
+        self.lrtext.show()
+        self.lroption1.show()
+        self.lrtext1.show()
+        self.lroption2.show()
+        self.lrcombo.show()
+
+    def lrhide(self):
+        self.lroption.hide()
+        self.lrtext.hide()
+        self.lroption1.hide()
+        self.lrtext1.hide()
+        self.lroption2.hide()
+        self.lrcombo.hide()
 
     def makeOptionGrid(self):
         self.optiongrid = QGridLayout()
@@ -179,18 +212,9 @@ class TrainerGUI(QWidget):
         self.lrcombo.addItems(['newton-cg', 'lbfgs', 'sag', 'saga'])
         self.optiongrid.addWidget(self.lrcombo, 0, 1)
 
-        self.dloption.hide()
-        self.dltext.hide()
-        self.svmoption.hide()
-        self.svmcombo.hide()
-        self.svmoption1.hide()
-        self.svmtext1.hide()
-        self.lroption.hide()
-        self.lrtext.hide()
-        self.lroption1.hide()
-        self.lrtext1.hide()
-        self.lroption2.hide()
-        self.lrcombo.hide()
+        self.dlhide()
+        self.svmhide()
+        self.lrhide()
         
     def trainStart(self):
         groupbox = QGroupBox('Training')
@@ -233,7 +257,76 @@ class TrainerGUI(QWidget):
         self.te2.setText(fname[0])
 
     def startButton(self):
-        pass
+        self.startbtn.setDisabled(True)
+        try:
+            pct = float(self.pcttext.text())
+
+            if pct <= 0 or pct >= 1:
+                raise ValueError
+
+            sav = self.te1.text()
+            if sav == '':
+                raise FilePathError()
+            
+            scaler = self.te2.text()
+            if scaler == '':
+                raise FilePathError()
+
+            arguments = self.getProperties()
+
+            self.combineData()
+            FD = trainer.Trainer(self.le.text())
+            FD.training(train_pct=pct, output_path=sav, output_scaler_path=scaler,
+                        model_name=ModelNames(self.cb.currentIndex()), properties=arguments)
+
+            FD.predict_current_model()
+
+        except FileNotFoundError:
+            msgbox = QMessageBox()
+            msgbox.setText('Please select data correctly')
+            msgbox.exec()
+        except ValueError:
+            msgbox = QMessageBox()
+            msgbox.setText('Please write values correctly')
+            msgbox.exec()
+        except FilePathError:
+            msgbox = QMessageBox()
+            msgbox.setText('Please select output path correctly')
+            msgbox.exec()
+
+        self.startbtn.setDisabled(False)
+
+    def combineData(self):
+        pathlist = self.te.toPlainText().split('\n')
+        with open(self.le.text(), 'a', newline='') as original_data:
+            if pathlist != ['']:
+                filewriter = csv.writer(original_data)
+                for path in pathlist:
+                    with open(path, 'r', newline='') as new_data:
+                        filereader = csv.reader(new_data)
+                        header = next(filereader)
+                        for row in filereader:
+                            filewriter.writerow(row)
+    
+    def getProperties(self):
+        properties = dict()
+
+        if self.cb.currentText() == 'Random Forest':
+            properties[self.rfoption.text()] = int(self.rftext.text())
+
+        elif self.cb.currentText() == 'Autoencoded Deep Learning':
+            properties[self.dloption.text()] = int(self.dltext.text())
+
+        elif self.cb.currentText() == 'Support Vector Machine':
+            properties[self.svmoption.text()] = self.svmcombo.currentText()
+            properties[self.svmoption1.text()] = float(self.svmtext1.text())
+
+        elif self.cb.currentText() == 'Logistic Regression':
+            properties[self.lroption.text()] = float(self.lrtext.text())
+            properties[self.lroption1.text()] = int(self.lrtext1.text())
+            properties[self.lroption2.text()] = self.lrcombo.currentText()
+
+        return properties
 
 
 if __name__ == '__main__':
